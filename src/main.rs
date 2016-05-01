3abab8cc0d86227bfac1166a6cf2ce33a79411b9
James <dong.james.sept14@gmail.com>
2016-05-01 11:16:22 -0500

    Add addition and subtraction

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use nom::{digit, IResult};
 use std::str;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Expression {
     Value(f64),
     Exp(Box<Expression>, Box<Expression>),
@@ -17,11 +17,11 @@ enum Expression {
     Sub(Box<Expression>, Box<Expression>),
 }
 
-named!(parens<Expression>, delimited!(char!('('), /* expr */fac, char!(')')));
+named!(parens<Expression>, dbg_dmp!(delimited!(char!('('), expr, char!(')'))));
 
-named!(number<f64>, map_res!(map_res!(digit, str::from_utf8), FromStr::from_str));
+named!(number<f64>, dbg_dmp!(map_res!(map_res!(digit, str::from_utf8), FromStr::from_str)));
 
-named!(atom<Expression>, alt!(number => {Expression::Value} | parens));
+named!(atom<Expression>, dbg_dmp!(alt!(number => {Expression::Value} | parens)));
 
 named!(exp<Expression>, dbg_dmp!(chain!(lhs: atom ~ rhs: preceded!(char!('^'), exp)?, ||{
     match (lhs, rhs) {
@@ -31,11 +31,31 @@ named!(exp<Expression>, dbg_dmp!(chain!(lhs: atom ~ rhs: preceded!(char!('^'), e
     }
 })));
 
-named!(fac<Expression>, dbg_dmp!(chain!(first: exp ~ others: many0!(tuple!(alt!(char!('*') | char!('/')), exp)), ||{
-    others.into_iter().fold(first, |lhs, (op, rhs)| simplify1(match op { '*' => Expression::Mul(Box::new(lhs), Box::new(rhs)), '/' => Expression::Div(Box::new(lhs), Box::new(rhs)), _ => Expression::Mul(Box::new(lhs), Box::new(rhs))}))
-})));
+named!(fac<Expression>, dbg_dmp!(
+        chain!(first: exp
+             ~ others: many0!(tuple!(
+                       alt!(char!('*') | char!('/')), exp)), ||
+    others.into_iter().fold(first, |lhs, (op, rhs)| simplify1(
+            match op {
+                '*' => Expression::Mul(Box::new(lhs), Box::new(rhs)),
+                '/' => Expression::Div(Box::new(lhs), Box::new(rhs)),
+                _   => Expression::Mul(Box::new(lhs), Box::new(rhs))
+            }))
+)));
 
-named!(input<Expression>, dbg_dmp!(chain!(res: fac ~ char!('?'), ||{res})));
+named!(expr<Expression>, dbg_dmp!(
+        chain!(first: fac
+             ~ others: many0!(tuple!(
+                       alt!(char!('+') | char!('-')), fac)), ||
+    others.into_iter().fold(first, |lhs, (op, rhs)| simplify1(
+            match op {
+                '+' => Expression::Add(Box::new(lhs), Box::new(rhs)),
+                '-' => Expression::Sub(Box::new(lhs), Box::new(rhs)),
+                _   => Expression::Add(Box::new(lhs), Box::new(rhs))
+            }))
+)));
+
+named!(input<Expression>, dbg_dmp!(chain!(res: expr ~ char!('?'), ||{res})));
 
 fn simplify1(expr: Expression) -> Expression {
     match expr {
@@ -48,11 +68,36 @@ fn simplify1(expr: Expression) -> Expression {
     }
 }
 
+macro_rules! test_expr {
+    ( $x:expr, $v: expr) => (assert_eq!(input(concat!($x, "?").as_bytes()), IResult::Done(&b""[..], Expression::Value($v))));
+}
+
+#[test]
+fn test_exponents() {
+    test_expr!("2^1^5", 2.0);
+}
+
+#[test]
+fn test_muldiv() {
+    test_expr!("2*3", 6.0);
+    test_expr!("3/2", 1.5);
+    test_expr!("3/2*4", 6.0);
+    test_expr!("2^2*3", 12.0);
+}
+
+#[test]
+fn test_addsub() {
+    test_expr!("1+1", 2.0);
+    test_expr!("3-2", 1.0);
+    test_expr!("3-2+3", 4.0);
+    test_expr!("2^3*4-5", 27.0);
+}
+
 fn main() {
-    println!("A: {:?}", exp(b"1?"));
-    println!("A: {:?}", fac(b"1*1?"));
-    println!("A: {:?}", fac(b"1/2*3?"));
-    println!("A: {:?}", fac(b"1^1?"));
-    println!("A: {:?}", fac(b"1^1^1?"));
-    println!("Result: {:?}", fac(b"2/3^5*2^1^2?"));
+    println!("A: {:?}", input(b"1?"));
+    println!("A: {:?}", input(b"1*1?"));
+    println!("A: {:?}", input(b"1/2*3?"));
+    println!("A: {:?}", input(b"1^1?"));
+    println!("A: {:?}", input(b"1^1^1?"));
+    println!("Result: {:?}", input(b"2/3^5*2^1^2?"));
 }
