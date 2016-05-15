c8fae25ceaf2615eab1a87ab485477422d5fa449
James Dong <dong.james.sept14@gmail.com>
2016-05-15 11:33:45 -0500

    Finish up UnitValue

--- src/uval.rs
+++ src/uval.rs
@@ -2,6 +2,8 @@ use unit::*;
 use value::*;
 use rational::OverflowError;
 use std::cmp;
+use std::ops::{Add,Sub,Mul,Div,Neg};
+use std::fmt;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct UnitValue {
@@ -48,7 +50,7 @@ impl UnitValue {
     pub fn add(&self, other: &UnitValue) -> Result<UnitValue, ArithmeticError> {
         if self.unit == other.unit {
             Ok(UnitValue {
-                value: try!(self.value.add(&other.value)),
+                value: try!((&self.value).add(&other.value)),
                 unit: self.unit,
             })
         } else {
@@ -58,7 +60,7 @@ impl UnitValue {
     pub fn sub(&self, other: &UnitValue) -> Result<UnitValue, ArithmeticError> {
         if self.unit == other.unit {
             Ok(UnitValue {
-                value: try!(self.value.sub(&other.value)),
+                value: try!((&self.value).sub(&other.value)),
                 unit: self.unit,
             })
         } else {
@@ -67,34 +69,82 @@ impl UnitValue {
     }
     pub fn mul(&self, other: &UnitValue) -> Result<UnitValue, ArithmeticError> {
         Ok(UnitValue {
-            value: try!(self.value.mul(&other.value)),
-            unit: try!(self.unit.add(&other.unit)),
+            value: try!((&self.value).mul(&other.value)),
+            unit: try!((&self.unit).add(&other.unit)),
         })
     }
     pub fn div(&self, other: &UnitValue) -> Result<UnitValue, ArithmeticError> {
         Ok(UnitValue {
-            value: try!(self.value.div(&other.value)),
-            unit: try!(self.unit.sub(&other.unit)),
+            value: try!((&self.value).div(&other.value)),
+            unit: try!((&self.unit).sub(&other.unit)),
         })
     }
     pub fn pow(&self, other: &UnitValue) -> Result<UnitValue, ArithmeticError> {
         if other.unitless() {
             if self.unitless() {
                 Ok(UnitValue {
-                    value: try!(self.value.pow(&other.value)),
+                    value: try!((&self.value).pow(&other.value)),
                     unit: Unit::zero(),
                 })
             } else {
                 match other.value.get_exact() {
                     Some(e) => Ok(UnitValue {
-                        value: try!(self.value.pow(&other.value)),
-                        unit: try!(self.unit.mul(e)),
+                        value: try!((&self.value).pow(&other.value)),
+                        unit: try!((&self.unit).mul(e)),
                     }),
                     None => Err(ArithmeticError::UnitError),
                 }
             }
         } else {
             Err(ArithmeticError::UnitError)
+        }
+    }
+}
+
+impl Add for UnitValue {
+    type Output = UnitValue;
+    fn add(self, other: UnitValue) -> UnitValue {
+        (&self).add(&other).unwrap()
+    }
+}
+
+impl Sub for UnitValue {
+    type Output = UnitValue;
+    fn sub(self, other: UnitValue) -> UnitValue {
+        (&self).sub(&other).unwrap()
+    }
+}
+
+impl Mul for UnitValue {
+    type Output = UnitValue;
+    fn mul(self, other: UnitValue) -> UnitValue {
+        (&self).mul(&other).unwrap()
+    }
+}
+
+impl Div for UnitValue {
+    type Output = UnitValue;
+    fn div(self, other: UnitValue) -> UnitValue {
+        (&self).div(&other).unwrap()
+    }
+}
+
+impl Neg for UnitValue {
+    type Output = UnitValue;
+    fn neg(self) -> UnitValue {
+        UnitValue {
+            value: -self.value,
+            unit: self.unit,
+        }
+    }
+}
+
+impl fmt::Display for UnitValue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        if self.unitless() {
+            write!(f, "{}", self.value)
+        } else {
+            write!(f, "{} {:?}", self.value, self.unit)
         }
     }
 }
